5bd6839e4312328bdbb3ab14b7cc0a780248807a
Garrett Hempy <43591380+ghempy@users.noreply.github.com>
2020-12-02 21:31:41 -0700

    final kindergarten reading decorator

--- Funi/app/src/main/java/com/example/funi/KindergartenReadingDecorator.java
+++ Funi/app/src/main/java/com/example/funi/KindergartenReadingDecorator.java
@@ -10,16 +10,16 @@ public class KindergartenReadingDecorator extends QuizDecorator{
     }
 
     private QuizInterface setQuestions(QuizInterface decoratedQuiz) {
-        decoratedQuiz.addQuestion("A", new ArrayList<>(Arrays.asList("A", "B", "C", "D")), "a");
-        decoratedQuiz.addQuestion("E", new ArrayList<>(Arrays.asList("E", "F", "G", "H")), "e");
-        decoratedQuiz.addQuestion("I", new ArrayList<>(Arrays.asList("I", "J", "K", "L")), "i");
-        decoratedQuiz.addQuestion("M", new ArrayList<>(Arrays.asList("M", "N", "O", "P")), "m");
-        decoratedQuiz.addQuestion("Q", new ArrayList<>(Arrays.asList("Q", "R", "S", "T")), "q");
-        decoratedQuiz.addQuestion("U", new ArrayList<>(Arrays.asList("U", "V", "W", "X")), "u");
-        decoratedQuiz.addQuestion("Y", new ArrayList<>(Arrays.asList("Y", "Z", "A", "B")), "y");
-        decoratedQuiz.addQuestion("ABCD", new ArrayList<>(Arrays.asList("ACBD","ABCD","ADCB","BACD")), "abcd");
-        decoratedQuiz.addQuestion("EFGH", new ArrayList<>(Arrays.asList("EFGH","HGFE","FGHE","EGFH")), "efgh");
-        decoratedQuiz.addQuestion("IJKL", new ArrayList<>(Arrays.asList("LKJI","KJIL","IKJL","IJKL")), "ijkl");
+        decoratedQuiz.addQuestion("hat", new ArrayList<>(Arrays.asList("hat", "pat", "cat", "bat")), "hat");
+        decoratedQuiz.addQuestion("cat", new ArrayList<>(Arrays.asList("bat", "cat", "cta", "fat")), "cat");
+        decoratedQuiz.addQuestion("car", new ArrayList<>(Arrays.asList("car", "bar", "rac", "arc")), "car");
+        decoratedQuiz.addQuestion("jar", new ArrayList<>(Arrays.asList("jra", "raj", "car", "jar")), "jar");
+        decoratedQuiz.addQuestion("bus", new ArrayList<>(Arrays.asList("but", "bus", "sub", "bes")), "bus");
+        decoratedQuiz.addQuestion("ham", new ArrayList<>(Arrays.asList("ham", "hame", "home", "hom")), "ham");
+        decoratedQuiz.addQuestion("book", new ArrayList<>(Arrays.asList("cook", "book", "buuk", "cuuk")), "book");
+        decoratedQuiz.addQuestion("bird", new ArrayList<>(Arrays.asList("bird", "burd", "berd", "bord")), "bird");
+        decoratedQuiz.addQuestion("ball", new ArrayList<>(Arrays.asList("baull", "bawl", "ball", "boll")), "ball");
+        decoratedQuiz.addQuestion("rat", new ArrayList<>(Arrays.asList("tat", "rat", "art", "tar")), "rat");
         return decoratedQuiz;
     }
 
