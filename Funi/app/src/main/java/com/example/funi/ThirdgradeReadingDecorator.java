7c03d4eff9b35ab4ba3b9b4158f174ee9b790555
SerenaLutterodt <47258616+SerenaLutterodt@users.noreply.github.com>
2020-12-03 10:32:06 -0700

    Updating 3rd Grade Reading To ProperQuiz

--- Funi/app/src/main/java/com/example/funi/ThirdgradeReadingDecorator.java
+++ Funi/app/src/main/java/com/example/funi/ThirdgradeReadingDecorator.java
@@ -10,16 +10,26 @@ public class SecondgradeReadingDecorator extends QuizDecorator{
     }
 
     private QuizInterface setQuestions(QuizInterface decoratedQuiz) {
-        decoratedQuiz.addQuestion("library", new ArrayList<>(Arrays.asList("library", "liebrary", "lybrary", "librari")), "library");
-        decoratedQuiz.addQuestion("pirate", new ArrayList<>(Arrays.asList("pirate", "pilot", "pyrate", "pirit")), "pirate");
-        decoratedQuiz.addQuestion("officer", new ArrayList<>(Arrays.asList("oficer", "officer", "offiser", "ophicer")), "officer");
-        decoratedQuiz.addQuestion("printer", new ArrayList<>(Arrays.asList("prenter", "printer", "prentir", "printa")), "printer");
-        decoratedQuiz.addQuestion("science", new ArrayList<>(Arrays.asList("siense", "seyeince", "syence", "science")), "science");
-        decoratedQuiz.addQuestion("walking", new ArrayList<>(Arrays.asList("walking", "wahking", "walcing", "wallking")), "walking");
-        decoratedQuiz.addQuestion("chicken", new ArrayList<>(Arrays.asList("chiken", "chicken", "thicken", "chikken")), "chicken");
-        decoratedQuiz.addQuestion("factory", new ArrayList<>(Arrays.asList("factory", "facktory", "facrty", "phactory")), "factory");
-        decoratedQuiz.addQuestion("kitchen", new ArrayList<>(Arrays.asList("citchen", "kitchen", "kittchen", "cittchen")), "kitchen");
-        decoratedQuiz.addQuestion("treasure", new ArrayList<>(Arrays.asList("tregure", "trehjur", "treasure", "tresure")), "treasure");
+        //butterfly.png
+        decoratedQuiz.addQuestion("butterfly", new ArrayList<>(Arrays.asList("buterfly", "butterfly", "betterfly", "butterfli")), "butterfly");
+        //banana.png
+        decoratedQuiz.addQuestion("banana", new ArrayList<>(Arrays.asList("baannaanna", "banana", "bahnahnah", "bannana")), "banana");
+        //sandwich.png
+        decoratedQuiz.addQuestion("sandwich", new ArrayList<>(Arrays.asList("sandwhich", "sanwich", "sandwich", "sandwitch")), "sandwich");
+        //jellyfish.png
+        decoratedQuiz.addQuestion("jellyfish", new ArrayList<>(Arrays.asList("jellyfesh", "jellifish", "jallyfish", "jellyfish")), "jellyfish");
+        //globe.png
+        decoratedQuiz.addQuestion("globe", new ArrayList<>(Arrays.asList("globe", "gloobe", "glob", "glowb")), "globe");
+        //airport.png
+        decoratedQuiz.addQuestion("airport", new ArrayList<>(Arrays.asList("aerport", "airport", "aeroport", "aireport")), "airport");
+        //farmer.png
+        decoratedQuiz.addQuestion("farmer", new ArrayList<>(Arrays.asList("phamir", "farmir", "farmer", "pharmer")), "farmer");
+        //birthday.png
+        decoratedQuiz.addQuestion("birthday", new ArrayList<>(Arrays.asList("burthday", "berthday", "birfday", "birthday")), "birthday");
+        //backpack.png
+        decoratedQuiz.addQuestion("backpack", new ArrayList<>(Arrays.asList("bacpac", "backpack", "bakpak", "baacpack")), "backpack");
+        //mountain.png
+        decoratedQuiz.addQuestion("mountain", new ArrayList<>(Arrays.asList("Mountan", "Mowntain", "Mountain", "Mowntin")), "mountain");
         return decoratedQuiz;
     }
 
