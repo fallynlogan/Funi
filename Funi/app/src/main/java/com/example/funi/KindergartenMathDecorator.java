7d7b46a7917212e07e63d5f53341197ae2cc755e
Garrett Hempy <43591380+ghempy@users.noreply.github.com>
2020-12-02 21:32:36 -0700

    final kinder math decorator

--- Funi/app/src/main/java/com/example/funi/KindergartenMathDecorator.java
+++ Funi/app/src/main/java/com/example/funi/KindergartenMathDecorator.java
@@ -10,16 +10,16 @@ public class KindergartenMathDecorator extends QuizDecorator {
     }
 
     private QuizInterface setQuestions(QuizInterface decoratedQuiz) {
-        decoratedQuiz.addQuestion("1", new ArrayList<>(Arrays.asList("0", "1", "2", "3")), "math1");
-        decoratedQuiz.addQuestion("4", new ArrayList<>(Arrays.asList("3", "8", "4", "2")), "math4");
-        decoratedQuiz.addQuestion("9", new ArrayList<>(Arrays.asList("6", "7", "9", "4")), "math9");
-        decoratedQuiz.addQuestion("6", new ArrayList<>(Arrays.asList("9", "2", "6", "5")), "math6");
-        decoratedQuiz.addQuestion("3", new ArrayList<>(Arrays.asList("3", "5", "9", "4")), "math3");
-        decoratedQuiz.addQuestion("10", new ArrayList<>(Arrays.asList("0", "2", "1", "10")), "math10");
-        decoratedQuiz.addQuestion("5", new ArrayList<>(Arrays.asList("0", "7", "10", "5")), "math5");
-        decoratedQuiz.addQuestion("7", new ArrayList<>(Arrays.asList("0", "7", "4", "2")), "math7");
-        decoratedQuiz.addQuestion("2", new ArrayList<>(Arrays.asList("2", "3", "4", "5")), "math2");
-        decoratedQuiz.addQuestion("8", new ArrayList<>(Arrays.asList("7", "8", "9", "10")), "math8");
+        decoratedQuiz.addQuestion("14", new ArrayList<>(Arrays.asList("14", "34", "4", "15")), "kmath1");
+        decoratedQuiz.addQuestion("9", new ArrayList<>(Arrays.asList("8", "9", "7", "10")),  "kmath2");
+        decoratedQuiz.addQuestion("0", new ArrayList<>(Arrays.asList("18", "1", "0", "9")), "kmath3");
+        decoratedQuiz.addQuestion("39", new ArrayList<>(Arrays.asList("50", "401", "41", "39")), "kmath4");
+        decoratedQuiz.addQuestion("94", new ArrayList<>(Arrays.asList("94", "95", "106", "104")), "kmath5");
+        decoratedQuiz.addQuestion("100", new ArrayList<>(Arrays.asList("1", "100", "50", "10")), "kmath6");
+        decoratedQuiz.addQuestion("88", new ArrayList<>(Arrays.asList("44", "4444", "88", "888")), "kmath7");
+        decoratedQuiz.addQuestion("27", new ArrayList<>(Arrays.asList("21", "1", "28", "27")), "kmath8");
+        decoratedQuiz.addQuestion("13", new ArrayList<>(Arrays.asList("13", "130", "1000", "10")), "kmath9");
+        decoratedQuiz.addQuestion("68", new ArrayList<>(Arrays.asList("86", "338", "68", "8")), "kmath10");
         return decoratedQuiz;
     }
 }
