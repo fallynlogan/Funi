bd7831b70f0c6e0c0b64ca97d84fb56038e81de3
Garrett Hempy <43591380+ghempy@users.noreply.github.com>
2020-12-02 21:34:50 -0700

    final second grade math decorator

--- Funi/app/src/main/java/com/example/funi/SecondgradeMathDecorator.java
+++ Funi/app/src/main/java/com/example/funi/SecondgradeMathDecorator.java
@@ -10,16 +10,30 @@ public class SecondgradeMathDecorator extends QuizDecorator {
     }
 
     private QuizInterface setQuestions(QuizInterface decoratedQuiz) {
-        decoratedQuiz.addQuestion("1", new ArrayList<>(Arrays.asList("0", "1", "2", "3")), "math1");
-        decoratedQuiz.addQuestion("4", new ArrayList<>(Arrays.asList("3", "8", "4", "2")), "math4");
-        decoratedQuiz.addQuestion("9", new ArrayList<>(Arrays.asList("6", "7", "9", "4")), "math9");
-        decoratedQuiz.addQuestion("6", new ArrayList<>(Arrays.asList("9", "2", "6", "5")), "math6");
-        decoratedQuiz.addQuestion("3", new ArrayList<>(Arrays.asList("3", "5", "9", "4")), "math3");
-        decoratedQuiz.addQuestion("10", new ArrayList<>(Arrays.asList("0", "2", "1", "10")), "math10");
-        decoratedQuiz.addQuestion("5", new ArrayList<>(Arrays.asList("0", "7", "10", "5")), "math5");
-        decoratedQuiz.addQuestion("7", new ArrayList<>(Arrays.asList("0", "7", "4", "2")), "math7");
-        decoratedQuiz.addQuestion("2", new ArrayList<>(Arrays.asList("2", "3", "4", "5")), "math2");
-        decoratedQuiz.addQuestion("8", new ArrayList<>(Arrays.asList("7", "8", "9", "10")), "math8");
+        // question 1
+        decoratedQuiz.addQuestion("195", new ArrayList<>(Arrays.asList("951", "519", "195", "150")), "secondmath1");
+        //question 2
+        decoratedQuiz.addQuestion("600", new ArrayList<>(Arrays.asList("660", "640", "650", "670")), "secondmath2");
+        // question 3
+        decoratedQuiz.addQuestion("210", new ArrayList<>(Arrays.asList("610", "710", "810", "210")), "secondmath3");
+        // question 4
+        decoratedQuiz.addQuestion("807", new ArrayList<>(Arrays.asList("710", "702", "87", "807")), "secondmath4");
+        //question 5
+        decoratedQuiz.addQuestion("306", new ArrayList<>(Arrays.asList("706", "806", "306", "506")), "secondmath5");
+        // question 6
+        decoratedQuiz.addQuestion("100", new ArrayList<>(Arrays.asList("100", "200", "300", "10")), "secondmath6");
+        //question 7
+        decoratedQuiz.addQuestion("20", new ArrayList<>(Arrays.asList("20", "10", "100", "200")), "secondmath7");
+        //question 8
+        decoratedQuiz.addQuestion("70", new ArrayList<>(Arrays.asList("170", "180", "80", "70")), "secondmath8");
+        // question 9
+        decoratedQuiz.addQuestion("9", new ArrayList<>(Arrays.asList("999", "99", "9", "8")), "secondmath9");
+        // question 10
+        decoratedQuiz.addQuestion("2", new ArrayList<>(Arrays.asList("12", "21", "2", "5")), "secondmath10");
+        // question 11
+        decoratedQuiz.addQuestion("6", new ArrayList<>(Arrays.asList("6", "7", "66", "77")), "secondmath11");
+        // question 12
+        decoratedQuiz.addQuestion("2", new ArrayList<>(Arrays.asList("6", "3", "2", "4")), "secondmath12");
         return decoratedQuiz;
     }
 }
