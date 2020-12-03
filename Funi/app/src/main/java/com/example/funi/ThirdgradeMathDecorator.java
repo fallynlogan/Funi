323e8fcf0c4ea9569fab4aaf4ebb25d87617fef2
Garrett Hempy <43591380+ghempy@users.noreply.github.com>
2020-12-02 21:35:59 -0700

    final third grade math decorator

--- Funi/app/src/main/java/com/example/funi/ThirdgradeMathDecorator.java
+++ Funi/app/src/main/java/com/example/funi/ThirdgradeMathDecorator.java
@@ -10,16 +10,30 @@ public class ThirdgradeMathDecorator extends QuizDecorator {
     }
 
     private QuizInterface setQuestions(QuizInterface decoratedQuiz) {
-        decoratedQuiz.addQuestion("1", new ArrayList<>(Arrays.asList("0", "1", "2", "3")), "math1");
-        decoratedQuiz.addQuestion("4", new ArrayList<>(Arrays.asList("3", "8", "4", "2")), "math4");
-        decoratedQuiz.addQuestion("9", new ArrayList<>(Arrays.asList("6", "7", "9", "4")), "math9");
-        decoratedQuiz.addQuestion("6", new ArrayList<>(Arrays.asList("9", "2", "6", "5")), "math6");
-        decoratedQuiz.addQuestion("3", new ArrayList<>(Arrays.asList("3", "5", "9", "4")), "math3");
-        decoratedQuiz.addQuestion("10", new ArrayList<>(Arrays.asList("0", "2", "1", "10")), "math10");
-        decoratedQuiz.addQuestion("5", new ArrayList<>(Arrays.asList("0", "7", "10", "5")), "math5");
-        decoratedQuiz.addQuestion("7", new ArrayList<>(Arrays.asList("0", "7", "4", "2")), "math7");
-        decoratedQuiz.addQuestion("2", new ArrayList<>(Arrays.asList("2", "3", "4", "5")), "math2");
-        decoratedQuiz.addQuestion("8", new ArrayList<>(Arrays.asList("7", "8", "9", "10")), "math8");
+        //ans 223
+        decoratedQuiz.addQuestion("223", new ArrayList<>(Arrays.asList("222", "223", "332", "123")), "thirdmath1");
+        // ans 440
+        decoratedQuiz.addQuestion("440", new ArrayList<>(Arrays.asList("440", "640", "450", "470")), "thirdmath2");
+        // ans 21
+        decoratedQuiz.addQuestion("21", new ArrayList<>(Arrays.asList("51", "20", "21", "31")), "thirdmath3");
+        // ans 23
+        decoratedQuiz.addQuestion("23", new ArrayList<>(Arrays.asList("23", "32", "232", "323")), "thirdmath4");
+        // ans 90
+        decoratedQuiz.addQuestion("90", new ArrayList<>(Arrays.asList("900", "90", "800", "80")), "thirdmath5");
+        // ans 25
+        decoratedQuiz.addQuestion("25", new ArrayList<>(Arrays.asList("51", "25", "52", "255")), "thirdmath6");
+        // ans 36
+        decoratedQuiz.addQuestion("36", new ArrayList<>(Arrays.asList("63", "636", "36", "363")), "thirdmath7");
+        // ans 25
+        decoratedQuiz.addQuestion("25", new ArrayList<>(Arrays.asList("25", "2", "5", "52")), "thirdmath8");
+        // ans 16
+        decoratedQuiz.addQuestion("16", new ArrayList<>(Arrays.asList("61", "16", "51", "15")), "thirdmath9");
+        // ans 7
+        decoratedQuiz.addQuestion("7", new ArrayList<>(Arrays.asList("77", "7", "676", "6")), "thirdmath10");
+        // ans 2
+        decoratedQuiz.addQuestion("2", new ArrayList<>(Arrays.asList("222", "22", "3", "2")), "thirdmath11");
+        // ans 3
+        decoratedQuiz.addQuestion("3", new ArrayList<>(Arrays.asList("3", "33", "2", "22")), "thirdmath12");
         return decoratedQuiz;
     }
 }
